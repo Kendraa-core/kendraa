375ce1adde85412aa6b7dc9662caf3d3f6e7b638
Mithul <mithul.231me335@nitk.edu.in>
2025-08-28 23:24:33 +0530

    Add text labels to navigation items and prominent search bar in header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -27,6 +27,7 @@ import {
   QuestionMarkCircleIcon,
   ChartBarIcon,
   PlusIcon,
+  MagnifyingGlassIcon,
 } from '@heroicons/react/24/outline';
 import { useNotifications } from '@/contexts/NotificationContext';
 import { formatRelativeTime } from '@/utils/formatRelativeTime';
@@ -102,7 +103,14 @@ export default function Header({ onRightSidebarToggle }: HeaderProps) {
               
               {/* Search Bar */}
               <div className="hidden sm:block flex-1 max-w-xs ml-2">
-                <UserSearch />
+                <div className="relative">
+                  <input
+                    type="text"
+                    placeholder="Search Kendraa..."
+                    className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg bg-gray-50 text-gray-900 focus:ring-2 focus:ring-azure-500 focus:border-transparent outline-none text-sm"
+                  />
+                  <MagnifyingGlassIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400" />
+                </div>
               </div>
             </div>
 
@@ -116,13 +124,14 @@ export default function Header({ onRightSidebarToggle }: HeaderProps) {
                       key={item.name}
                       href={item.href}
                       className={cn(
-                        'flex flex-col items-center justify-center w-20 h-12 rounded-lg transition-colors relative',
+                        'flex flex-col items-center justify-center w-24 h-16 rounded-lg transition-colors relative',
                         pathname === item.href
                           ? 'text-azure-500'
                           : 'text-gray-600 hover:text-azure-500'
                       )}
                     >
                       <item.icon className="w-6 h-6 mb-1" />
+                      <span className="text-xs font-medium">{item.name}</span>
                       {pathname === item.href && (
                         <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-8 h-0.5 bg-azure-500 rounded-full"></div>
                       )}
